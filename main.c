e498631df2961c8d9367ae3efe5205a60afd6702
Viktor <todosijevicviktor998@gmail.com>
2021-08-15 12:25:32 +0200

    Signed-off-by: Viktor <todosijevicviktor998@gmail.com>

--- main.c
+++ main.c
@@ -15,11 +15,11 @@ void LCD_write(uint8_t rs, uint8_t val);
 void LCD_write_nibble(uint16_t nibble[], uint8_t val);
 
 #define TIMER_PERIOD (1023)     //  (2^10 - 1 ) / 2^15 ms = ~31ms
-#define CLOCK_PERIDO (1048576)	//	1s?
-#define t_AS (1)				//	1us? > 40ns
-#define t_DSW (1)				//	1us? > 80ns
-#define t_H (1)					//	1us? > 10ns
-#define t_BETWEEN_NIBBLES (1)	//	just to be sure
+#define CLOCK_PERIDO (1048576)  //  1s?
+#define t_AS (1)                //  1us? > 40ns
+#define t_DSW (1)               //  1us? > 80ns
+#define t_H (1)                 //  1us? > 10ns
+#define t_BETWEEN_NIBBLES (1)   //  just to be sure
 
 // TODO Osposobiti LCD i napisati nesto na njemu, bez toga ne mogu da debagujem ADC
 
@@ -44,116 +44,118 @@ uint16_t buttons[] = {
 #define enable lcd_gpios[1]
 
 uint16_t lcd_gpios[] = {
-					   BIT2,	//	register select
-					   BIT3,	//	enable
-					   BIT4,	//	4
-					   BIT5,	//	5
-					   BIT6,	//	6
-					   BIT7		//	7
+                       BIT2,    //  register select
+                       BIT3,    //  enable
+                       BIT4,    //  4
+                       BIT5,    //  5
+                       BIT6,    //  6
+                       BIT7     //  7
 };
 
 uint16_t lcd_data[] = {
-					   BIT4,	//	4
-					   BIT5,	//	5
-					   BIT6,	//	6
-					   BIT7		//	7
+                       BIT4,    //  4
+                       BIT5,    //  5
+                       BIT6,    //  6
+                       BIT7     //  7
 };
 
 void LCD_init()
 {
-	int i;
-	for(i = 0; i < sizeof(lcd_gpios) / sizeof(lcd_gpios[0]); i++)
-	{
-		P8DIR |= lcd_gpios[i];		//	set the LCD intended GPIO pins for output
-		P8OUT &= ~lcd_gpios[i];		//	set them to LOW
-	}
+    int i;
+    for(i = 0; i < sizeof(lcd_gpios) / sizeof(lcd_gpios[0]); i++)
+    {
+        P8DIR |= lcd_gpios[i];      //  set the LCD intended GPIO pins for output
+        P8OUT &= ~lcd_gpios[i];     //  set them to LOW
+    }
 }
 
-//	https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
+//  https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
 
 void LCD_write_nibble(uint16_t nibble[], uint8_t val)
 {
-	P8OUT |= enable;	//	enable rising edge
+    P8OUT |= enable;    //  enable rising edge
 
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        //  if val at position is 1 set, else clear
+        if (val & nibble[i])
+            P8OUT |= lcd_data[i];
+        else
+            P8OUT &= ~ lcd_data[i];
+    }
 
-	int i;
-	for (i = 0; i < 4; i++)
-	{	
-		//	if val at position is 1 set, else clear
-		if (val & nibble[i])
-			P8OUT |= lcd_data[i];
-		else
-			P8OUT &= ~ lcd_data[i];
-	}
+    __delay_cycles(t_DSW);
 
-	__delay_cycles(t_DSW);	
-
-	P8OUT &= ~enable;	//	enable falling edge
+    P8OUT &= ~enable;   //  enable falling edge
 }
 
 void LCD_write(uint8_t rs, uint8_t val)
 {
 
-	if (rs == 1)						// set/clear RS depending on instruction or data
-		P8OUT |= register_select;
-	else if (rs == 0)
-		P8OUT &= ~register_select;
-	
-	__delay_cycles(t_AS);				//	timing characteristics? page 58
+    if (rs == 1)                        // set/clear RS depending on instruction or data
+    {
+        P8OUT |= register_select;
+    }
+    else if (rs == 0)
+    {
+        P8OUT &= ~register_select;
+    }
+
+    __delay_cycles(t_AS);               //  timing characteristics? page 58
 
 
-	uint16_t upper[] = {BIT7, BIT6, BIT5, BIT4};	//	set upper 4 bits
+    uint16_t upper[] = {BIT7, BIT6, BIT5, BIT4};    //  set upper 4 bits
 
-	LCD_write_nibble(upper, val);
+    LCD_write_nibble(upper, val);
 
-	__delay_cycles(t_BETWEEN_NIBBLES);
+    __delay_cycles(t_BETWEEN_NIBBLES);
 
 
-	uint16_t lower[] = {BIT3, BIT2, BIT1, BIT0};	//	set lower 4 bits
+    uint16_t lower[] = {BIT3, BIT2, BIT1, BIT0};    //  set lower 4 bits
 
-	LCD_write_nibble(lower, val);
+    LCD_write_nibble(lower, val);
 
 }
 
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	//  stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   //  stop watchdog timer
 
     //  2 LEDs  out
-	int i;
-	for (i = 0; i < sizeof(led_bits) / sizeof(led_bits[0]); i++)    //  iterate through array
-	{
-	    P4DIR |= led_bits[i];   //  set for out
-	    P4OUT &= ~led_bits[i];  //  turn LEDs off initially
-	}
+    int i;
+    for (i = 0; i < sizeof(led_bits) / sizeof(led_bits[0]); i++)    //  iterate through array
+    {
+        P4DIR |= led_bits[i];   //  set for out
+        P4OUT &= ~led_bits[i];  //  turn LEDs off initially
+    }
 
-	//  2 buttons in
-	for (i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++)
-	{
-	    P2DIR &= ~buttons[i];    //  clear for in
-	    P2IES |= buttons[i];     //  falling edge
-	    P2IFG &= ~buttons[i];    //  clear interrupt flag
-	    P2IE |= buttons[i];      //  enable interrupt
-	}
+    //  2 buttons in
+    for (i = 0; i < sizeof(buttons) / sizeof(buttons[0]); i++)
+    {
+        P2DIR &= ~buttons[i];    //  clear for in
+        P2IES |= buttons[i];     //  falling edge
+        P2IFG &= ~buttons[i];    //  clear interrupt flag
+        P2IE |= buttons[i];      //  enable interrupt
+    }
 
-	//  initialize timer A
-	TA0CCR0 = TIMER_PERIOD;     //  debounce period
-	TA0CCTL0 = CCIE;            //  enable CCR0 interrupt
-	TA0CTL = TASSEL__ACLK;      //  clock
+    //  initialize timer A
+    TA0CCR0 = TIMER_PERIOD;     //  debounce period
+    TA0CCTL0 = CCIE;            //  enable CCR0 interrupt
+    TA0CTL = TASSEL__ACLK;      //  clock
 
-	LCD_init();
-	LCD_write(1, character);
+    LCD_init();
+    LCD_write(1, character);
 
 
-	__enable_interrupt();   //  global interrupt enable
+    __enable_interrupt();   //  global interrupt enable
 
-	while(1)
-	{
+    while(1)
+    {
+    }
 
-	}
-	
-	return 0;
+    return 0;
 }
 
 /**
@@ -174,7 +176,7 @@ void __attribute__ ((interrupt(PORT2_VECTOR))) P2ISR(void)
             P2IFG &= ~buttons[i];           //  clear button flag
             P2IE &= ~buttons[i];            //  disable button interrupts
             pressed_button = buttons[i];    //  specify which button was pressed
-			corresponding_led = led_bits[i];
+            corresponding_led = led_bits[i];
 
         }
     return;
@@ -189,7 +191,7 @@ void __attribute__ ((interrupt(PORT2_VECTOR))) P2ISR(void)
 void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) CCR0ISR (void)
 {
     if ((P2IN & pressed_button) == 0)   // if button is still pressed start conversion
-		P4OUT ^= corresponding_led;		// toggle correspoding led
+        P4OUT ^= corresponding_led;     // toggle correspoding led
 
         
 
